a1b4211c9cea8304bfd162963bd02f3c4bc62177
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-05-25 22:59:32 +0800

    testing server client using testgame

--- basic_pygame/network.py
+++ basic_pygame/network.py
@@ -4,11 +4,15 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "SERVER_IP"
+        self.server = "192.168.137.1"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+        print(self.pos)
+        
+    def getPos(self):
+        return self.pos
+
 
     def connect(self):
         try:
--- basic_pygame/server.py
+++ basic_pygame/server.py
@@ -2,8 +2,7 @@ import socket
 from _thread import *
 import sys
 
-#input IPV4 here
-server = "127.0.0.1"
+server = "192.168.137.1"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,32 +15,50 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
 
-def threaded_client(conn):
-    conn.send(str.encode("Connected to server"))
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+#starting position of player 1 & 2
+pos = [(0,0),(100,100)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Received: ", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
                 print("Sending : ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         except:
             break
 
     print("Lost connection")
     conn.close()
 
-
+#save number of player
+currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    #counter to keep track current player
+    currentPlayer += 1
