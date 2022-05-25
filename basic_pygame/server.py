8e2696236b049ad2ab41bcd4b9a29eadcd06bda3
gilbertk27 <64368640+gilbertk27@users.noreply.github.com>
2022-05-25 21:33:38 +0800

    added network.py

--- basic_pygame/server.py
+++ basic_pygame/server.py
@@ -18,7 +18,7 @@ print("Waiting for a connection, Server Started")
 
 
 def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+    conn.send(str.encode("Connected to server"))
     reply = ""
     while True:
         try:
