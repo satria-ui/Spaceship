3778441fd6b3188b7c590421d32e16fc253d596c
Adam Satria <satriaadam04@gmail.com>
2022-06-15 15:31:03 +0200

    finalized

--- basic_pygame/client2.py
+++ basic_pygame/client2.py
@@ -2,22 +2,25 @@ import pygame
 import os
 from network import Network
 from player import Player
+pygame.mixer.init()
+pygame.font.init()
 
 width = 800
 height = 800
 ENEMY_HIT = pygame.USEREVENT + 1
 PLAYER_HIT = pygame.USEREVENT + 2
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Spaceship")
+pygame.display.set_caption("CLIENT 2")
 spaceback = pygame.image.load('./assets/spaceback.png')
 spaceback = pygame.transform.scale(spaceback,(width,height))
 
+WINNER_FONT = pygame.font.SysFont('comicsans', 80)
 MAX_BULLET = 3
 # BULLET_COLOR = (255,0,0) #merah
 BULLET_VEL = 5
 
 
-def draw_window(win,player, enemy, bullet_counts, enemy_health, player_health):
+def draw_window(win,player, enemy, bullet_counts, bullet_enemy_counts, enemy_health, player_health):
     win.fill((100,100,100))
     #for background
     i = 0
@@ -34,16 +37,22 @@ def draw_window(win,player, enemy, bullet_counts, enemy_health, player_health):
     ship2 = pygame.transform.rotate(pygame.transform.scale(ship2,(100,100)), enemy.rotation)
     
     # win.blit(ship1, (player.x,player.y))
-    player.draw(win, ship, bullet_counts, enemy_health, player_health)
+    player.draw(win, ship, bullet_counts, bullet_enemy_counts, enemy_health, player_health)
     print("PLAYER: " + str(player_health))
     # win.blit(ship2, (player2.x, player2.y))
-    enemy.draw(win, ship2, bullet_counts, enemy_health, player_health)
+    enemy.draw(win, ship2, bullet_counts, bullet_enemy_counts, enemy_health, player_health)
     print("ENEMY: " + str(enemy_health))
     
     # for bullet in bullet_counts:
     #     pygame.draw.rect(win, BULLET_COLOR, bullet)
         
     pygame.display.update()
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, True, (255,255,255))
+    win.blit(draw_text, (width/2 - draw_text.get_width()/2, height/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
 
 def bullet_handle(bullet_counts, p2):
     for bullet in bullet_counts:
@@ -56,13 +65,26 @@ def bullet_handle(bullet_counts, p2):
         # elif p.player.colliderect(bullet):
         #     pygame.event.post(pygame.event.Event(PLAYER_HIT))
         #     bullet_counts.remove(bullet)
+        
+def bullet_enemy_handle(bullet_enemy_counts, p):
+    for bullet2 in bullet_enemy_counts:
+        bullet2.x += BULLET_VEL
+        if bullet2.x > width:
+            bullet_enemy_counts.remove(bullet2)
+        elif pygame.Rect.colliderect(bullet2,p.player):
+            pygame.event.post(pygame.event.Event(PLAYER_HIT))
+            bullet_enemy_counts.remove(bullet2)
+        # elif p.player.colliderect(bullet):
+            # pygame.event.post(pygame.event.Event(PLAYER_HIT))
+            # bullet_enemy_counts.remove(bullet)
 
 def main():
     PLAYER_HEIGHT = 100
     PLAYER_WIDTH = 100
     bullet_counts = []
-    ENEMY_HEALTH = 10
-    PLAYER_HEALTH = 10
+    bullet_enemy_counts = []
+    enemy_health = 10
+    player_health  = 10
     
     run = True
     n = Network()
@@ -80,16 +102,26 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(bullet_counts) < MAX_BULLET:
                         bullet = pygame.Rect(p.x + int(p.width/2), p.y + int(p.height/2), 10, 5)
-                        bullet_counts.append(bullet)
-            
+                        p.bullet_pos.append(bullet)
+                        
             if event.type == ENEMY_HIT:
-                ENEMY_HEALTH -= 1
+                enemy_health -= 1
             if event.type == PLAYER_HIT:
-                PLAYER_HEALTH -= 1
+                player_health -= 1
         
-        draw_window(win, p, p2, bullet_counts, ENEMY_HEALTH, PLAYER_HEALTH) 
+        draw_window(win, p, p2, p.bullet_pos, p2.bullet_pos, enemy_health, player_health)
         p.move()
-        bullet_handle(bullet_counts, p2)
+        bullet_handle(p.bullet_pos,p2)
+        bullet_enemy_handle(p2.bullet_pos,p)
+        
+        winner_text = ""
+        if player_health <= 0:
+            winner_text = "You Lose"
+        if enemy_health <= 0:
+            winner_text = "You Win"
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
                     
 if __name__ == "__main__":
     main()
