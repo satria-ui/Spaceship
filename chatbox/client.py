7ab91279e6a0cf4585e9734ec44bf0a3a1eb6756
Adam Satria <satriaadam04@gmail.com>
2022-06-15 15:32:46 +0200

    update

--- chatbox/client.py
+++
@@ -1,6 +0,0 @@
-
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = int(input("Connect on port: "))
-s.connect(("192.168.137.1", port))
-this = input("Press any key to exit:")
